64ad02e944dd9936d22816c3f237bdd91f245389
Parth Sharma <72144581+ParthSharma21@users.noreply.github.com>
2020-10-02 17:30:20 +0530

    Update leapyear.c

--- leapyear.c
+++ leapyear.c
@@ -5,13 +5,12 @@ int main ()
     int y;
     printf("Enter the year\n");
     scanf("%d", &y);
-    if ((y % 4 == 0) || (y % 100 == 0) || (y % 400 == 0))
+    if(y % 100 == 0 && y % 400 == 0)
+        printf("%d is a leap year\n", y);//this shows leap-year.
+    else if (y % 4 == 0)
     	printf("%d is a leap year\n", y);//this shows leap-year.
-    
     else
-    {
         printf("%d is not a leap year\n", y);//this shows non-leap year.
-    }
     return 0;
 
 }
